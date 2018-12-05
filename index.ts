df56b6a31cffc44bc6ccd40fa8040c06e0a1ed2f
Andy Chou <acchou2@gmail.com>
2018-12-04 23:18:37 -0800

    update

--- index.ts
+++ index.ts
@@ -5,11 +5,11 @@ import * as uuid from "uuid/v4";
 
 const dir = tmpdir();
 const cloudifyDir = join(dir, "cloudify");
-console.log(`mkdir ${cloudifyDir}`);
-mkdirSync(cloudifyDir, { recursive: true });
+//console.log(`mkdir ${cloudifyDir}`);
+//mkdirSync(cloudifyDir, { recursive: true });
 
 const uuidDir = join(cloudifyDir, uuid());
 console.log(`mkdir ${uuidDir}`);
-mkdirSync(uuidDir);
+mkdirSync(uuidDir, { recursive: true });
 
 console.log("done");
