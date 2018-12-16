8ff4a908597c3c6eb68542ff0ed6830e4c5300de
Andy Chou <acchou2@gmail.com>
2018-12-15 23:18:03 -0800

    upgrade packages, try multiple auth methods

--- index.ts
+++ index.ts
@@ -2,12 +2,28 @@ import { google } from "googleapis";
 
 export async function initializeGoogleServices() {
     console.log(`getClient`);
-    const auth = await google.auth.getClient({
-        // scopes: ["https://www.googleapis.com/auth/cloud-platform"]
-    });
+    const auth1 = await google.auth
+        .getClient({
+            scopes: ["https://www.googleapis.com/auth/cloud-platform"]
+        })
+        .catch(console.error);
+    console.log(`auth1: ${auth1}`);
+
+    const response = await google.auth.getApplicationDefault().catch(console.error);
+    console.log(`auth2: ${response}`);
+
+    const token = await google.auth.getAccessToken().catch(console.error);
+    console.log(`auth3: ${token}`);
+
+    const project = await google.auth.getProjectId();
+    console.log(`%O`, { project });
+
+    const creds = await google.auth.getCredentials();
+    console.log(`%O`, { email: creds.client_email });
+
     console.log(`google.options`);
 
-    google.options({ auth });
+    // google.options({ auth: auth1 });
     console.log(`returning `);
 
     return {
@@ -23,13 +39,6 @@ export async function main() {
         throw err;
     });
     console.log(`%O`, { services });
-
-    console.log(`Listing functions`);
-    const functionsResponse = await services.cloudFunctions.projects.locations.functions.list();
-    const functions = functionsResponse.data.functions || [];
-    for (const func of functions) {
-        console.log(`%O`, { func });
-    }
 }
 
 main();
