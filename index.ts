a5992dd754e21695e5ba72df31dcc8e8749b19c7
Andy Chou <acchou2@gmail.com>
2018-12-15 21:46:07 -0800

    instrument

--- index.ts
+++ index.ts
@@ -1,21 +1,27 @@
 import { google } from "googleapis";
 
 export async function initializeGoogleServices() {
+    console.log(`getClient`);
     const auth = await google.auth.getClient({
         scopes: ["https://www.googleapis.com/auth/cloud-platform"]
     });
+    console.log(`google.options`);
+
     google.options({ auth });
+    console.log(`returning `);
+
     return {
         cloudFunctions: google.cloudfunctions("v1"),
-        // pubsub: google.pubsub("v1"),
-        // cloudBilling: google.cloudbilling("v1"),
         google
     };
 }
 
 export async function main() {
     console.log(`Creating google services`);
-    const services = await initializeGoogleServices();
+    const services = await initializeGoogleServices().catch(err => {
+        console.error(`ERROR1: ${err}`);
+        throw err;
+    });
     console.log(`%O`, { services });
 
     console.log(`Listing functions`);
