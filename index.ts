b39be226b96bad82a02bcd50abd6e7477fbd344e
Andy Chou <acchou2@gmail.com>
2018-12-15 21:47:26 -0800

    no scope

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ import { google } from "googleapis";
 export async function initializeGoogleServices() {
     console.log(`getClient`);
     const auth = await google.auth.getClient({
-        scopes: ["https://www.googleapis.com/auth/cloud-platform"]
+        // scopes: ["https://www.googleapis.com/auth/cloud-platform"]
     });
     console.log(`google.options`);
 
