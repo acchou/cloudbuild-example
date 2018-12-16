dcbab99134e0984b5457664bdb7fa78eb09b2f01
Andy Chou <acchou2@gmail.com>
2018-12-15 17:26:33 -0800

    change to cloud functions example

--- index.ts
+++ index.ts
@@ -1,15 +1,27 @@
-import { tmpdir } from "os";
-import { mkdirSync } from "fs";
-import { join } from "path";
-import * as uuid from "uuid/v4";
+import { google } from "googleapis";
 
-const dir = tmpdir();
-const cloudifyDir = join(dir, "cloudify");
-//console.log(`mkdir ${cloudifyDir}`);
-//mkdirSync(cloudifyDir, { recursive: true });
+export async function initializeGoogleServices() {
+    const auth = await google.auth.getClient({
+        scopes: ["https://www.googleapis.com/auth/cloud-platform"]
+    });
+    google.options({ auth });
+    return {
+        cloudFunctions: google.cloudfunctions("v1"),
+        pubsub: google.pubsub("v1"),
+        cloudBilling: google.cloudbilling("v1"),
+        google
+    };
+}
 
-const uuidDir = join(cloudifyDir, uuid());
-console.log(`mkdir ${uuidDir}`);
-mkdirSync(uuidDir, { recursive: true });
+export async function main() {
+    console.log(`Creating google services`);
+    const services = await initializeGoogleServices();
+    console.log(`%O`, { services });
 
-console.log("done");
+    console.log(`Listing functions`);
+    const functionsResponse = await services.cloudFunctions.projects.locations.functions.list();
+    const functions = functionsResponse.data.functions || [];
+    for (const func of functions) {
+        console.log(`%O`, { func });
+    }
+}
